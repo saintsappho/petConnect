ebcf59de6d0726ff1d2c2ab04adf9bac942b214c
saintsappho <robinrosefleur@gmail.com>
2024-02-02 17:08:45 -0700

    reorganizing the frontend src^2 issue

---
+++ petConnect-react/frontend_2/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import HomeRoute from "./components/HomeRoute";
+import Login from "./components/Login";
+// import { getUsers } from "";
+
+function App() {
+  return (
+    <div>
+      <HomeRoute />
+      {/* {getUsers()} */}
+      {/* <Login /> */}
+    </div>
+  );
+}
+
+export default App;
