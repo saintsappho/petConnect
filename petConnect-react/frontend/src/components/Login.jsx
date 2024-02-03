52b1c886c57e780dee030ed4d88b82af446b1729
Tandra Malm <tandra_malm@hotmail.com>
2024-02-02 17:30:38 -0700

    cleaned up duplicates

--- petConnect-react/frontend/src/components/Login.jsx
+++ petConnect-react/frontend/src/components/Login.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:petConnect-react/frontend/src/components/Login.jsx
 import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
 
@@ -7,33 +6,9 @@ const LoginButton = () => {
 
   return <button onClick={() => loginWithRedirect()}>Log In</button>;
 };
-=======
-import React from "react";
-import "../styles/Login.css";
-
-export default function Login() {
-  return (
-    <div className="Login">
-      <form action="">
-        <h1>Login</h1>
-        <div className="user-pass-box">
-          <input type="text" placeholder="Username" required />
-        </div>
-        <div className="user-pass-box">
-          <input type="password" placeholder="Password" required />
-        </div>
-
-        <div className="remember-pass">
-          <label>
-            <input type="checkbox" />
-            Remeber me
-          </label>
-        </div>
->>>>>>> main:petConnect-react/frontend_2/src/components/Login.jsx
 
 export default LoginButton;
 
-<<<<<<< HEAD:petConnect-react/frontend/src/components/Login.jsx
 // import React from 'react';
 // import '../styles/Login.css';
 
@@ -64,15 +39,3 @@ export default LoginButton;
 //     </div>
 //   )
 // }
-=======
-        <div className="register-forgot">
-          <p>
-            Not already signed up? <a href="#">Register now!</a>
-          </p>
-          <a href="#">Forgot password?</a>
-        </div>
-      </form>
-    </div>
-  );
-}
->>>>>>> main:petConnect-react/frontend_2/src/components/Login.jsx
--- petConnect-react/frontend/src/main.jsx
+++ petConnect-react/frontend/src/main.jsx
@@ -1,10 +1,20 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
-// import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
+import App from './App';
+import HomeRoute from './components/HomeRoute';
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+<Auth0Provider
+    domain="dev-6ebu1wieb31ofvv3.us.auth0.com"
+    clientId="dXjoqKdp9BVih5gK0EATd0gtYDZbr1IM"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
+  <HomeRoute />
+  </Auth0Provider >
+  
+);
