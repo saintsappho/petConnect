8bd25e423260ee7ebbea2ae4a90711afcbddae0e
Tandra Malm <tandra_malm@hotmail.com>
2024-02-06 01:56:22 -0700

    cleaned up commented out code from messaging files

--- petConnect-react/backend/routes/directMessages.js
+++ petConnect-react/backend/routes/directMessages.js
@@ -39,45 +39,6 @@ server.listen(4000, () => {
   console.log("Server is running on port 4000");
 });
 
-// io.on("connection", (socket) => {
-//   console.log('User is connected: ${socket.id}');
-
-//   socket.on("join_chat", (data) => {
-//     console.log('User joined chat: ${data}');
-//     socket.join(data);
-
-//     io.emit('user join chat', data);
-//   });
-
-//   socket.on("send_message", (data) => {
-//     socket.to(currentChat.chat_ID).emit('send_message', response.data);
-//   });
-// });
-
-// socket.on("send_message", async (data) => {
-//   try {
-//     // Send the message to the chat room
-//     socket.to(data.chat_id).emit('new_message', data);
-
-//     const newMessage = await getMessages();
-
-//     // Check if newMessage and newMessage.rows exist before accessing properties
-//     if (newMessage && newMessage.rows && newMessage.rows.length > 0) {
-//       // Emit the new message to all clients in the chat room
-//       io.to(data.chat_ID).emit('new_message', newMessage.rows[0]);
-//     } else {
-//       console.error("Error: No messages found or invalid message structure");
-//     }
-//   } catch (error) {
-//     console.error("Error sending message:", error);
-//   }
-// });
-// });
-
-// server.listen(4000, () => {
-//   console.log("Server is running on port 4000");
-// });
-
 router.post("/", (req, res) => {
   console.log("POST request received at /");
 
--- petConnect-react/frontend/src/components/messaging/SendMessage.jsx
+++ petConnect-react/frontend/src/components/messaging/SendMessage.jsx
@@ -28,39 +28,6 @@ export default function SendMessage({ currentChat, socket }) {
     setNewMessage("");
   };
 
-// import React, { useState } from 'react';
-// import axios from 'axios';
-// import './SendMessage.scss';
-
-// export default function SendMessage({ currentChat, onMessageSent }) {
-//   const [newMessage, setNewMessage] = useState("");
-
-//   const handleSendMessage = async () => {
-//     try {
-//       // Check if currentChat exists before accessing properties
-//       if (!currentChat || !currentChat.chat_id) {
-//         console.error("No chat selected");
-//         return;
-//       }
-  
-//       // Send the message to the backend
-//       const response = await axios.post('http://localhost:8080/directMessages', {
-//         chat_ID: currentChat.chat_id,
-//         sender: currentChat.user1_ID,
-//         receiver: currentChat.user2_ID,
-//         message: newMessage,
-//       });
-
-//       // Notify the parent component that a message has been sent
-//       onMessageSent(response.data);
-  
-//       // Clear the input field
-//       setNewMessage("");
-//     } catch (error) {
-//       console.error("Error sending message:", error);
-//     }
-//   };
-
   return (
     <div>
       <textarea
