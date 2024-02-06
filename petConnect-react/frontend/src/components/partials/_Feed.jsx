b3ffd0de192f7d600c9cd0c4da8be278724d31d1
saintsappho <robinrosefleur@gmail.com>
2024-02-05 21:29:01 -0700

    css fix.

--- petConnect-react/frontend/src/components/partials/_Feed.jsx
+++ petConnect-react/frontend/src/components/partials/_Feed.jsx
@@ -1,4 +1,6 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
+
 // dependencies
 import { useState, useEffect } from "react";
 // components
@@ -7,21 +9,6 @@ import PetPost from "./_PetPost.jsx";
 
 export default function Feed(props) {
   const { user, posts, error } = props;
-
-  // when this useEffect starts working, remove the reverse from the array.map in the return statement
-  // useEffect(() => {
-  //   const eventSource = new EventSource("http://localhost:8080/posts/stream");
-  //   console.log(
-  //     "remember to remove the reverse from the array.map in the return statement",
-  //   );
-  //   eventSource.onmessage = function (event) {
-  //     const newPost = JSON.parse(event.data);
-  //     setPosts((prevPosts) => [newPost, ...prevPosts]); // Add newPost to the beginning
-  //   };
-  //   return () => {
-  //     eventSource.close(); // Close connection when component unmounts
-  //   };
-  // }, [posts]); // Notice that we add setPosts here to ensure correctness. Adjust if necessary, especially if setPosts changes or is re-created on re-renders.
 
   if (error) {
     return (
