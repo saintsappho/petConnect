a24372182283eb2de7ad01f316a8601f7e6f71f7
Dylan Palin <dylanpalin@gmail.com>
2024-02-07 04:50:21 -0700

    can't figure out why modal won't close and widget won't render, petData undefined

--- petConnect-react/frontend/src/components/NavBar.jsx
+++ petConnect-react/frontend/src/components/NavBar.jsx
@@ -1,53 +1,39 @@
-import "../styles/TopNav.scss"
+import useFetchData from "../hooks/useFetchData.js";
+import "../styles/TopNav.scss";
 import LogoutButton from "./Logout";
 import Modal from "./modals/Modal.jsx";
-
-// import { useState } from "
-// import React from 'react';
-//The NavBar component will be a fixed navigation bar at the top of the page. It will contain links to the home page, the scheduling page, the messaging page, and either the login page or the user's profile page and logout button.
-//The NavBar will be visible on all pages of the app, and will be responsive to different screen sizes.
 
 export default function Navbar({ petData, handlePetListSelect, openCurrentUserModal, user }) {
   return (
     <nav className="top-nav-bar">
-
-      <div className="top-nav-bar__logo">
-      </div>
-
+      <div className="top-nav-bar__logo"></div>
       <div className="top-nav-bar__title">
         <h1>Petconnect</h1>
       </div>
-
       <div className="top-nav-bar__links">
-      <li className="top-nav-bar__item">
-        <a href="/">Home</a>
-      </li>
-
-      <li className="top-nav-bar__item">
-        <a href="/">About</a>
-      </li>
-
-      <li>
-      <form onSubmit={e => e.preventDefault()}> 
-        <select name="pets" onChange={handlePetListSelect}>
-        <option value="">Select a pet</option>
-          {petData.map(pet => (
-            <option key={pet.pet_id} value={pet.pet_id}>{pet.name}</option>
-          ))}
-        </select>
-      </form>
-      </li>
-
-      <li className="top-nav-bar__item">
-        <form onSubmit={e => e.preventDefault()}>
-        <button onClick={openCurrentUserModal} >{user.name}</button>
-        </form>
-      </li>
-
-      <li className="top-nav-bar__item">
-        <LogoutButton />
-      </li>
-
+        <li className="top-nav-bar__item">
+          <a href="/">Home</a>
+        </li>
+        <li className="top-nav-bar__item">
+          <a href="/">About</a>
+        </li>
+        <li className="top-nav-bar__item">
+          <button onClick={openCurrentUserModal}>{user.name}</button>
+        </li>
+        <li className="top-nav-bar__item">
+          <LogoutButton />
+        </li>
+        <li className="top-nav-bar__item">
+          <select name="pets" onChange={handlePetListSelect}>
+            <option value="">Select a pet</option>
+            {petData &&
+              (petData.map((pet) => (
+                <option key={pet.pet_id} value={pet.pet_id}>
+                  {pet.name}
+                </option>
+              )))}
+          </select>
+        </li>
       </div>
     </nav>
   );
--- petConnect-react/frontend/src/components/UserProfile.jsx
+++ petConnect-react/frontend/src/components/UserProfile.jsx
@@ -1,53 +1,64 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { useState } from "react";
+import PetListWidget from './PetListWidget.jsx';
 import DirectMessages from "./DirectMessages";
 
 //User Profile component will be a modal that pops up when the user clicks on "profile" in the top right corner of the NavBar. It will display the user's name, email, phone number, and a list of their pets.
 //The user's name, email, and phone number will be hardcoded for now, but the list of pets will be dynamic and will be pulled from the database.
 //the user's information will be able to be edited by the user, and the user will be able to add or remove pets from their list.
 
-export default function UserProfile ({ userId, handleConversationClick }) {
+export default function UserProfile({userId, handleConversationClick, petData}) {
   const { user, isAuthenticated, isLoading } = useAuth0();
-
   const [isDirectMessagesOpen, setDirectMessagesOpen] = useState(false);
 
   const openDirectMessages = () => {
     setDirectMessagesOpen(true);
   };
-
   const closeDirectMessages = () => {
     setDirectMessagesOpen(false);
   };
 
   if (isLoading) {
-    return <div>Loading ...<br/><img className="loading-cat" src="./src/assets/petProfileHead.gif" alt="Pet Profile Picture" /></div>;
+    return (
+      <div>
+        Loading ...<br />
+        <img
+          className="loading-cat"
+          src="./src/assets/petProfileHead.gif"
+          alt="Pet Profile Picture"
+        />
+      </div>
+    );
   }
 
   return (
     isAuthenticated && (
-    <div className="user-profile-container">
+      <div className="user-profile-container">
         <div className="profile__header">
-        <img className="profile__image" src={user.picture} alt={user.name} />
+          <img className="profile__image" src={user.picture} alt={user.name} />
           <h1 className="profile__name">{user.name}</h1>
-          <table className="profile__buttons"> 
+          <h2>{user.location}</h2>
+          <table className="profile__buttons">
             <thead>
               <tr>
                 <td>
-                  <button id="friendButton" onClick={()=>{console.log("add-friend")}}>Friend</button>
+                  <button id="friendButton" onClick={() => { console.log("add-friend") }}>Add Friend</button>
                 </td>
                 <td>
-                <button id="messageButton" onClick={openDirectMessages}>Message</button>
+                  <button id="messageButton" onClick={openDirectMessages}>Message</button>
                 </td>
               </tr>
             </thead>
-            
           </table>
           <h2>{user.location}</h2>
-            <article id="bio" >&quot;I&apos;m {user.name} and I own 3 pets, a golden retriever named Max, a cat named Benji, and a ferret named Snoopy!&quot;</article>
+          <article id="bio">&quot;I&apos;m {user.name} and I own 3 pets, a golden retriever named Max, a cat named Benji, and a ferret named Snoopy!&quot;</article>
         </div>
-
-      {isDirectMessagesOpen && <DirectMessages onClose={closeDirectMessages} />}
-    </div>
+        <div className="profileBody">
+          <h2 id="petSectionTitle">Pets!</h2>
+          <PetListWidget petData={petData} listPayload="currentUser" />
+        </div>
+        {isDirectMessagesOpen && <DirectMessages onClose={closeDirectMessages} />}
+      </div>
     )
-  )
+  );
 }
