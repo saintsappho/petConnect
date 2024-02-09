f6ba753643680d61dd4a623fb67b17912349a457
saintsappho <robinrosefleur@gmail.com>
2024-02-08 17:31:49 -0700

    Fixed timestamp issue with cloogey workaround. Polls now display their choices.

--- petConnect-react/frontend/src/components/partials/postcards/_Poll.jsx
+++ petConnect-react/frontend/src/components/partials/postcards/_Poll.jsx
@@ -3,25 +3,51 @@
 import useFormatDateTime from "../../../assets/helpers/formatDateTime";
 import useFetchData from "../../../hooks/useFetchData";
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Poll(props) {
   const { randomImage, petPost } = props;
   const [pollData, setPollData] = useState(null);
+  const [choices, setChoices] = useState([]);
   const [error, setError] = useState(null);
+  const [renderedChoices, setRenderedChoices] = useState(null);
   // console.log("Poll petPost: ", petPost);
   // console.log("pollData: ", pollData);
-  useFetchData(`http://localhost:8080/polls/${petPost.post_id}`, "polls", setPollData, setError);
+ 
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get(`http://localhost:8080/polls/${petPost.post_id}`);
+        console.log(`useFetchData got this data from polls`, response.data);
+        setPollData(response.data);
+        setChoices(response.data.choices);
+      } catch (error) {
+        console.error(`Error fetching data from polls`, error.message);
+        setError(error.message);
+      }
+    }
+    fetchData();
+  }, [petPost.post_id]);
+console.log("choices: ", choices);
+  useEffect(() => {
+    const renderedChoices = choices.map((choice) => {
+      return <button key={choice.choice_id} className="card__choice">{choice.choicetext}</button>
+    })
+    setRenderedChoices(renderedChoices);
+    console.log("renderedChoices 1: ", renderedChoices);
+  }, [choices])
+  console.log("renderedChoices 2: ", renderedChoices);
   return (
     <div className="card">
       <figure className="card__thumb">
         <img
-          src={petPost.image_file || randomImage()}
+          src={randomImage()}
           alt={petPost.content}
           className="card__image"
         ></img>
         <figcaption className="card__caption">
           <h2 className="card__title">{petPost.title}</h2>
-          <p className="card__snippet">{petPost.content}</p>
+          {renderedChoices}
           <a className="card__button">
             Vote Now!
           </a>
