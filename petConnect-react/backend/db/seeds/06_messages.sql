b861e8c49cd34f2d9dac0ffdb250a66fd81c0f84
Tandra Malm <tandra_malm@hotmail.com>
2024-02-12 13:55:44 -0700

    added photos to users seeds

--- petConnect-react/backend/db/seeds/06_messages.sql
+++ petConnect-react/backend/db/seeds/06_messages.sql
@@ -4,6 +4,8 @@ VALUES
   (1, 'Robin Fleur', 'John Deer', 'Hello! How are you?', '2024-01-01T12:00:00'),
   (1, 'Robin Fleur', 'John Deer', 'I heard you are in town!', '2024-01-01T12:15:00'),
   (2, 'Robin Fleur', 'Jane Plain', 'Hey freind, what a cool app hey!?', '2024-01-02T14:00:00'),
-  (3, 'Robin Fleur', 'Patrick Stump', 'Hey Patrick!', '2024-01-02T14:00:00')
+  (3, 'Robin Fleur', 'Patrick Stump', 'Hey Patrick!', '2024-01-02T14:00:00'),
+  (4, 'Robin Fleur', 'Hayley Williams', 'Hayley, do you want to go walk our dogs?', '2024-01-02T14:00:00'),
+  (5, 'Robin Fleur', 'Jason Ng', 'OMG I heard you got a new puppy?!?', '2024-01-02T14:00:00')
   -- Add more messages as needed
 ;
