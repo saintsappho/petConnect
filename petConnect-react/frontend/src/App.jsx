6fde8f642be97da43a5efdf25cf6d89e2145d323
Dylan Palin <dylanpalin@gmail.com>
2024-02-11 19:54:59 -0700

    fixed modal not opening

--- petConnect-react/frontend/src/App.jsx
+++ petConnect-react/frontend/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 // components
 import HomeRoute from './components/HomeRoute'
 import LoginButton from './components/Login'
+import LogoutButton from './components/Logout'
 import UserProfile from './components/UserProfile'
 import Loading from './components/Loading'
 import PetProfile from './components/PetProfile';
@@ -13,8 +14,12 @@ import Modal from './components/modals/Modal';
 import AuthProvider from './Auth0/AuthProvider';
 import useFetchData from './hooks/useFetchData'
 
+//styles
+import "./App.css";
+import "./styles/Modal.scss"
+
 function App() {
-  const { user, isLoading, isAuthenticated, getAccessTokenSilently } = useAuth0();
+  const { user, isAuthenticated, isLoading, getAccessTokenSilently } = useAuth0();
   const [modal, setModal] = useState(false);
   const [selectedPet, setSelectedPet] = useState(null);
   const [petData, setPetData] = useState([]);
@@ -24,6 +29,36 @@ function App() {
   const [userPets, setUserPets] = useState([]);
   const [error, setError] = useState(null);
   const userId = user?.sub;
+
+  // // Auth0 Token
+  // useEffect(() => {
+  //   const fetchAccessToken = async () => {
+  //     try {
+  //       if (isAuthenticated) {
+  //         const token = await getAccessTokenSilently();
+  //         setAccessToken(token);
+  //       }
+  //     } catch (error) {
+  //       console.error("Error fetching access token:", error);
+  //     }
+  //   };
+
+  //   fetchAccessToken();
+  // }, [isAuthenticated, getAccessTokenSilently]);
+
+  // animation for fancy button
+  var animateButton = function (e) {
+    e.preventDefault;
+    e.target.classList.remove('animate');
+    e.target.classList.add('animate');
+    setTimeout(function () {
+      e.target.classList.remove('animate');
+    }, 700);
+  };
+  var bubblyButtons = document.getElementsByClassName("bubbly-button");
+  for (var i = 0; i < bubblyButtons.length; i++) {
+    bubblyButtons[i].addEventListener('click', animateButton, false);
+  }
 
   // ensure modal is closed when user logs in/out
   useEffect(() => {
@@ -36,7 +71,7 @@ function App() {
   // fetch user's pet data for list
   useFetchData("http://localhost:8080/pets", "pets", setPetData, setFetchError);
 
-    // logic for navbar user pet list
+  // logic for navbar user pet list
   function handlePetListSelect(event, petId) {
     console.log('event: ', event);
     event.preventDefault();
@@ -54,32 +89,31 @@ function App() {
   }
 
   const openCurrentUserModal = (event) => {
-    setSelectedPet(null);
-    console.log('user: ', user);
-    console.log('userId: ', userId);
-    console.log('petData: ', petData);
-    console.log('accessToken: ', accessToken);
-    setModalContent(<UserProfile accessToken={accessToken} petData={petData} user={user} userId={userId} />); 
+        setSelectedPet(null);
+    // console.log('pet data: ', petData);
+
+    setPetData(petData);
+    setModalContent(<UserProfile accessToken={accessToken} petData={petData} user={user} userId={userId} />);
     openModal(event);
   };
 
   const openModal = (event) => {
     event.preventDefault();
-    setModal(true);
-  };
+          setModal(true);
+      };
 
   const closeModal = (event) => {
-    event.preventDefault();
+event.preventDefault();
     console.log('closing modal');
     setModalContent([]);
-    setModal(false);
-  };
+          setModal(false);
+      };
 
   return (
     <AuthProvider accessToken={accessToken}>
     <div className="App">
       <div>
-        {!user && <LoginButton className="login-button-login-page" />}
+{!user && <LoginButton className="login-button-login-page" />}
         {error && <p>Authentication Error</p>}
         {!error && isLoading && <Loading />}
         {!error && !isLoading && user && (
